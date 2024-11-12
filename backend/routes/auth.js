99788035227ecb9f293671451e0363de844ec7c0
SE7H <djenseth@yahoo.com>
2024-11-12 19:19:30 +0100

    fix: Clean up

--- backend/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const login = require('../controllers/authController');
-const isLoggedIn = require('../middlewares/isLoggedIn')
-
-router.post('/login', login);
-router.get('/check-auth', isLoggedIn)
-
-module.exports = router;
